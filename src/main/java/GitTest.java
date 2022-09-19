73d36ec97e73706befaab5580f450b66314748da
hfzbx <2289934894@qq.com>
2022-09-19 18:42:34 +0800

    hot-fix first commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
         System.out.println("hello Git!!!");
         System.out.println("hello Git!!!");
         System.out.println("hello Git!!!");
+        System.out.println("hello Git!!!");
     }
 }
