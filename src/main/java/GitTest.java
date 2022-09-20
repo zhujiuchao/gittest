d98d77ac70018ad8ed2dc026fd26559741970a55
hfzbx <2289934894@qq.com>
2022-09-20 15:05:09 +0800

    hot-fix second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
         System.out.println("hello Git!!!");
         System.out.println("hello Git!!!");
         System.out.println("hello Git!!!");
+        System.out.println("hot-fix test!!!");
     }
 }
